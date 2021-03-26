e1bb8b6d7ffc6ce8f7913aabc4443bb963473256
‪Abubakr Elghazawy‬‏ <abubakr8911@gmail.com>
2021-03-26 03:17:07 +0200

    react-native-day2

--- App.js
+++ App.js
@@ -1,21 +1,119 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text, Image, StyleSheet  } from 'react-native';
+import { Card, ListItem, Button, Icon, Divider } from 'react-native-elements'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+// That is Navigation Bar
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+//That is Home Screen Content
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+
+
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+    <Image
+      style={styles.workerAvatar}
+      source={require('./assets/images/worker.png')}
+    />
+    <Text>Worker Name</Text>
+    <Text>Worker Job</Text>
+    <Divider style={ styles.workerDivider } />
+  </View>
+
+
+
+  
   );
+  
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:50,
+    padding:15,
+    marginLeft:15,
   },
+  workerAvatar : {
+    width:60,
+    height:60,
+    
+  },
+  logo: {
+    width: 66,
+    height:58,
+  },
+  workerDivider : {
+   
+  }
+
 });
+
+
+
+export default App;
